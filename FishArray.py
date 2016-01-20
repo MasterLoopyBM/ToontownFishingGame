b8be95a64800b0e0699792814e5f5760a356d782
Master Loopy Bananamuffin <epicsqueebo@gmail.com>
2016-01-19 22:25:17 -0500

    Update FishArray.py
    Added Star Fish declarations.

--- FishArray.py
+++ FishArray.py
@@ -1,1 +1,2 @@
-FishDict = [['Balloon Fish', 'Red Balloon Fish', 'Water Balloon Fish'], ['Clown Fish', 'Sad Clown Fish']]
+FishDict = [['Balloon Fish', 'Red Balloon Fish', 'Water Balloon Fish'], ['Clown Fish', 'Sad Clown Fish'], ['Star Fish', 'Five Star Fish',
+'All Star Fish', 'Rock Star Fish', 'Shining Star Fish']]
